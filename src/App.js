261fd4896f1449789cfba7170ca5bb0194d3f2cb
Brandyn <stateofw@gmail.com>
2021-12-08 18:22:18 -0500

    mvp completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Users from './component/users'
 
 function App() {
   return (
     <div className="App">
-      Async Redux Project
+      <h1>Async Redux Project</h1>
+      <Users/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 import './index.css';
+import store from './store/store'
+import {Provider} from 'react-redux';
 
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+    <Provider store={store}>
+    <App />
+    </Provider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
